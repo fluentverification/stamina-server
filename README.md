b2cc4856c16721fe133c23e88271f9bdb9f27662
Josh Jeppson <joshua.jeppson@usu.edu>
2022-10-03 14:02:26 -0600

    Some more stuff for StaminaController

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # STAMINA for Servers
 
-This repository contains code to set up the STAMINA REST API through the Spring framework. It is not yet finished.
+This repository contains code to set up the STAMINA REST API through the Spring framework. It is not yet finished. Eventually we will add a Gradle build file.
--- src/StaminaController.java
+++ src/StaminaController.java
@@ -7,3 +7,37 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+// Our classes
+import org.staminachecker.server.StaminaQueries;
+import org.staminachecker.server.StaminaQuery;
+import org.staminachecker.server.StaminaQueryMap;
+
+@RestController
+@RequestMapping(path = "/queries")
+public class StaminaController {
+
+	@Autowired
+	private StaminaQueries staminaQueries;
+	
+	/**
+	 * Gets updates for a specific UID. Given a UID, gets any updates from the log
+	 * */
+	@GetMapping(
+		path = "/"
+		, produces = "application/json"
+		, consumes = "application/json"
+	)
+	public String getUpdate(@RequestBody String uid) {
+		// TODO
+	}
+	
+	@PostMapping(
+		path = "/"
+		, produces = "application/json"
+		, consumes = "application/json"
+	)
+	public ResponseEntity<Object> startJob(@RequestBody StaminaQuery query) {
+		// TODO
+	}
+}
