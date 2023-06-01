fa2f5e266867a0c357c33155a1a8ef181971d0e7
Josh Jeppson <joshua.jeppson@usu.edu>
2023-06-01 14:28:47 -0600

    Front end for REST API

--- src/Job.py
+++ src/Job.py
@@ -1,11 +1,20 @@
+'''
+TODO: 
+- Use Flask's file uploads (https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/) methods rather than trying to take file content as a string.
+- Save file into a folder <cwd>/<job-id>/model.prism, etc for prop file
+- Mount that folder into the docker container
+'''
 import docker
 from flask import jsonify
+from time import time
+from shutil import rmtree
+import os
 
 from settings import Settings
 
 client = docker.from_env()
 
-BASE_IMAGE="ifndefjosh/sstamina:v3.0_06330ce720011b1a4a1e8e1c740a601fa8d137ed"
+BASE_IMAGE="ifndefjosh/sstamina:qest23"
 client.images.pull(BASE_IMAGE)
 
 SSTAMINA="/opt/stamina-storm/build/sstamina"
@@ -23,8 +32,19 @@ def check_float(string):
 	except ValueError:
 		return False
 
+def safe_terminal_string(contents):
+	contents = contents.replace("\\", "\\\\")
+	contents = contents.replace('"', '\\"')
+	contents = contents.replace("'", "\\'")
+	contents = contents.replace("\n", "\\n")
+	return contents
+
+def create_command_write_file(contents, filename):
+	contents = safe_terminal_string(contents)
+	return f"tee {filename} < \"{contents}\""
+
 class Job:
-	def __init__(self, data, uid):
+	def __init__(self, data, uid, model_provided=True, prop_provided=True, path="", ip=None, in_container_path="/tmp/"):
 		'''
 	Data should be in JSON format
 		'''
@@ -32,13 +52,39 @@ class Job:
 		self.kappa = None
 		self.rkappa = None
 		self.window = None
+		self.creation_time = int(time())
+		self.path = path
+		self.ip = ip
 		args = self.__get_args_from_data(data)
-		# TODO: create model.prism and properties.csl in docker container
-		self.command = f"{SSTAMINA} {args} --help" # f"sstamina {args} model.prism properties.csl"
-		self.container = client.containers.run(BASE_IMAGE, self.command, detach=True)
+		# create model.prism and properties.csl in docker container
+		if model_provided and prop_provided:
+			if path == "":
+				print("Path should not be \"\"!")
+				return
+			#mod = create_command_write_file(data["model_file"], "model.prism")
+			#prop = create_command_write_file(data["prop_file"], "properties.csl")
+			#mount = docker.types.Mount(target=in_container_path, source=self.path, read_only=True)
+			self.command = f"{SSTAMINA} {args} {in_container_path}/model.prism {in_container_path}/prop.csl" # f"sstamina {args} model.prism properties.csl"
+			#full_command = f"{mod} && {prop} \\\n && cat model.prism"
+			#print(f"Full command for docker container:\n{full_command}")
+			self.has_inputs = True
+			self.container = client.containers.run(BASE_IMAGE
+				, self.command
+				, detach=True
+				, volumes={path:{'bind':in_container_path, 'mode':'rw'}}
+			)
+		# If model and properties files not specified, just get STAMINA version
+		else:
+			print("Either model or property file not provided!")
+			self.command = f"{SSTAMINA} -v"
+			self.has_inputs = False
+			self.container = None
 		self.uid = uid
-		
-	
+
+	def clean(self):
+		if os.path.isdir(self.path):
+			rmtree(self.path)
+
 	def __str__(self):
 		data = self.__json__()
 		return jsonify(data)
@@ -46,13 +92,17 @@ class Job:
 	def __json__(self, **options):
 		data = {}
 		data["uid"] = self.uid
-		data["status"] = self.container.status
-		data["command"] = self.command
-		data["method"] = self.method
-		data["kappa"] = self.kappa
-		data["rkappa"] = self.rkappa
-		data["window"] = self.window
-		data["logs"] = self.container.logs()
+		if self.container is not None:
+			data["status"] = self.container.status
+			data["logs"] = self.container.logs()
+			data["command"] = self.command
+			data["method"] = self.method
+			data["kappa"] = self.kappa
+			data["rkappa"] = self.rkappa
+			data["window"] = self.window
+		else:
+			data["status"] = "nonexistent"
+			data["message"] = "A model or properties file (which is required) was not provided"
 		return data
 
 	def __get_args_from_data(self, data):
@@ -100,3 +150,4 @@ class Job:
 
 	def kill(self):
 		self.container.stop()
+		self.clean()
--- src/app.py
+++ src/app.py
@@ -1,18 +1,63 @@
 #!flask/bin/python
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import secrets
 import sys
+from time import time, sleep
+from threading import Thread, Lock
+import os
+from datetime import datetime
 
-from settings import Settings
+from settings import Settings, EASTER_EGG
 from Job import Job
+
+logfile=sys.stdout
+
+def log(msg):
+	date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
+	print(f"[{date_string}]: {msg}", file=logfile)
+
 
 app = Flask(__name__)
 
 # List of all jobs and their associated
 jobs = {}
-
 ip_to_job = {}
+
+def periodically_clean_jobs():
+	'''
+The target thread which waits every so often and then cleans up old jobs
+	'''
+	try:
+		while True:
+			sleep(Settings.CLEANUP_INTERVAL)
+			clean_jobs()
+	except KeyboardInterrupt:
+		cur_time = int(time())
+		log(f"Removing temp directory")
+		for uid, job in jobs.items():
+			job.kill()
+			job.clean()
+
+def clean_jobs():
+	'''
+Clean up old jobs
+	'''
+	cur_time = int(time())
+	log("Cleaning old jobs:")
+	for uid, job in jobs.items():
+		age = cur_time - job.creation_time
+		if age >= Settings.MAX_ALLOWED_TIME_PER_JOB:
+			job.kill()
+			log(f"Killing job {uid}")
+		if age >= Settings.MAX_JOB_RETENTION_TIME:
+			ip = job.ip
+			job.clean()
+			del a[uid]
+			# Clean ip_to_job
+			cur_ip_joblist = ip_to_job[ip]
+			cur_ip_joblist.remove(job)
+			log(f"Removing job {uid}")
 
 def get_client_ip():
 	# Get the IP if not behind a proxy
@@ -23,27 +68,38 @@ def get_client_ip():
 
 def check_request(request, required_fields):
 	if not request.is_json:
-		return False, {"error": "Request should be in JSON format"}, 415
+		return False, "Request should be in JSON format", 415
 	request_json = request.get_json()
 	for field in required_fields:
 		if not field in request_json:
 			return False, {"error": f"Field \'{field}\' is required!"}, 400
 		
-def create_job(job_id, job_data=None):
+def create_job(job_id, job_data=None, model_provided=False, prop_provided=False, path=""):
+	#jobs_lock.acquire()
 	if len(jobs) > Settings.MAX_ALLOWED_JOBS:
+		#jobs_lock.release()
 		return f"Refused: too many total jobs running on server", 503
 	ip = get_client_ip()
+	job = None
 	if ip in ip_to_job and len(ip_to_job[ip]) > Settings.MAX_REQUESTS_PER_IP:
+		#jobs_lock.release()
 		return f"Refused: Too many jobs for IP address {ip}", 429
+		log(f"Refusing to create job for IP Address {ip}: They have too many active jobs")
 	elif ip in ip_to_job:
-		job = Job(job_data, job_id)
+		job = Job(job_data, job_id, model_provided, prop_provided, path, ip)
 		ip_to_job[ip].append(job)
 		jobs[job_id] = job
 	else:
-		job = Job(job_data, job_id)
+		job = Job(job_data, job_id, model_provided, prop_provided, path, ip)
 		ip_to_job[ip] = [job]
 		jobs[job_id] = job
-	return "Unimplemented", 501
+	log(f"Creating job with id {job_id} for IP address {ip}")
+	if job.has_inputs:
+		#jobs_lock.release()
+		return "Created", 201
+	else:
+		#jobs_lock.release()
+		return "No input", 400
 
 def authenticate(uname, passwd):
 	'''
@@ -61,26 +117,53 @@ def get_random_id():
 # When we upgrade flask version we can just use @app.post
 @app.route("/jobs", methods=["POST"])
 def post_jobs():
+	#jobs_lock.acquire()
 	# check to see if there is a job ID
-	if not request.is_json:
-		return {"error": "Request should be in JSON format"}, 415
 	request_json = request.get_json()
+	if request_json is None:
+		#jobs_lock.release()
+		return "Request body should be in JSON format (got no JSON!)", 415
 	if "id" in request_json:
-		return jobs[request_json["id"]].__json__()
+		if request_json["id"] in jobs:
+			log(f"Providing information about {request_json['id']} to ip {get_client_ip()}")
+			#jobs_lock.release()
+			return jobs[request_json["id"]].__json__()
+		else:
+			#jobs_lock.release()
+			return {"error": f"Job with id {request_json['id']} does not exist"}, 404
 	if "create" in request_json and request_json["create"].lower() == "true":
+		# Create a job ID
+		job_id = get_random_id()
+		# Get the model file
+		model_provided = "model_file" in request_json
+		# Get the properties file
+		prop_provided = "prop_file" in request_json
+		# Only create temporary directory if both files provided
+		path = ""
+		if model_provided and prop_provided:
+			# Create a temporary path
+			path = f"{Settings.TMP_DIRECTORY_LOCATION}/{job_id}/"
+			if not os.path.isdir(path):
+				os.makedirs(path)
+			with open(os.path.join(path, "model.prism"), "w") as m:
+				m.write(request_json["model_file"])
+			with open(os.path.join(path, "prop.csl"), "w") as p:
+				p.write(request_json["prop_file"])
 		# Create a job and run it
 		response = {}
-		job_id = get_random_id()
 		response["id"] = job_id
-		job_status, job_status_code = create_job(job_id, request_json)
+		job_status, job_status_code = create_job(job_id, request_json, model_provided, prop_provided, path)
 		response["status"] = job_status
+		#jobs_lock.release()
 		return response, job_status
 	# If not, they are trying to get all jobs. Currently, no authentication is supported for this, so it will fail always
 	check_request(request, ["username", "password"])
 	authenticated = authenticate(request["username"], request["password"])
 	if authenticated:
+		#jobs_lock.release()
 		return jsonify(jobs)
 	else:
+		#jobs_lock.release()
 		return {"error": "Authentication failure"}, 401
 
 @app.route("/jobs", methods=["GET"])
@@ -89,29 +172,74 @@ def get_jobs():
 
 @app.route("/myjobs", methods=["GET"])
 def get_my_jobs():
+	#jobs_lock.acquire()
 	ip = get_client_ip()
+	log(f"{ip} has asked for their active jobs")
 	if not ip in ip_to_job:
+		#jobs_lock.release()
 		return {"error": "This IP has no active jobs"}, 400
 	my_jobs = []
 	for job in ip_to_job[ip]:
 		my_jobs.append(job.__json__())
+	#jobs_lock.release()
 	return jsonify(my_jobs)
 
 @app.route("/myjobs", methods=["DELETE"])
 def delete_all_my_jobs():
+	#jobs_lock.acquire()
 	ip = get_client_ip()
+	log(f"{ip} has asked to DELETE all their active jobs")
 	if not ip in ip_to_job:
+		#jobs_lock.release()
 		return {"error": "This IP has no active jobs"}, 400
 	jobs_to_delete_count = len(ip_to_job[ip])
 	for job in ip_to_job[ip]:
-		# TODO: kill job
+		job.kill()
 		pass
 	ip_to_job.remove(ip)
+	#jobs_lock.release()
 	return {"message": f"Success! Deleted {jobs_to_delete_count} jobs!" }
 
+@app.route("/about", methods=["GET"])
+def about_stamina():
+	return """=================================================
+STAMINA - the STochiastic Approximate Model-checker for INfinite-state Systems
+https://staminachecker.org
+=================================================
+STAMINA is licensed under the GPLv3 and is developed at Utah State University
+=================================================
+This version of STAMINA is based on the Storm probabilistic model checking engine, also licensed under the GPLv3 and available at https://stormchecker.org
+=================================================
+"""
+
+# A little fun
 @app.route("/egg", methods=["GET"])
 def get_egg():
-	return {"egg": f"Easter Egg. Your IP Address is {get_client_ip()}"}
+	return f"There are four lights!\n{EASTER_EGG}"
+
+@app.route("/qapla", methods=["GET"])
+def get_qapla():
+	return f"<html><body><img src=https://upload.wikimedia.org/wikipedia/en/3/36/TNG-redemption_worf_and_gowron.png></img><br><h1>Qapla!</h1></body></html>"
+
+@app.route("/checkjob", methods=["POST"])
+def check_job():
+	'''
+Assumes the entire body of the request is the uid
+	'''
+	jid = request.get_data().decode("utf-8")
+	log(f"{get_client_ip()} has asked to get information about job {jid}")
+	if jid == "":
+		return "The /checkjob URI takes a plaintext request format and is intended to be used via terminal!", 415
+	if not jid in jobs:
+		return f"The ID {jid} does not exist or is not known!", 415
+	response = Response(jobs[jid].get_logs())
+	# TODO: should probably change this to allow only https://staminachecker.org
+	response.headers['Access-Control-Allow-Origin'] = '*'
+	return response
+
+#Create cleaning thread
+t = Thread(target=periodically_clean_jobs)
+t.start()
 
 if __name__=="__main__":
 	if "--debug" in sys.argv or "-d" in sys.argv:
