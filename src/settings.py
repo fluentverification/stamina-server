606fc84a10b961395d4173598340f2c503e608da
Josh Jeppson <joshua.jeppson@usu.edu>
2023-06-06 14:14:06 -0600

    Changed settings

--- src/settings.py
+++ src/settings.py
@@ -5,11 +5,11 @@ class Settings:
 	MAX_REQUESTS_PER_IP = 3
 	MAX_ALLOWED_JOBS = 100
 	MAX_ALLOWED_TIME_PER_JOB = 5 # seconds
-	MAX_JOB_RETENTION_TIME = 10 * 60 # 10 minutes 
-	#MAX_JOB_RETENTION_TIME = 10 * 60 * 60 # 10 hours
-	CLEANUP_INTERVAL = 5 * 60 # Five minutes
+	#MAX_JOB_RETENTION_TIME = 10 * 60 # 10 minutes 
+	MAX_JOB_RETENTION_TIME = 10 * 60 * 60 # 10 hours
+	#CLEANUP_INTERVAL = 5 * 60 # Five minutes
 	LOG_FILE = ""
-	#CLEANUP_INTERVAL = 24 * 60 * 60 # 24 hours
+	CLEANUP_INTERVAL = 24 * 60 * 60 # 24 hours
 	TMP_DIRECTORY_LOCATION = os.path.join(os.getcwd(), "api_data")
 	# STAMINA Settings
 	STAMINA_DEFAULT_METHOD = "reexploring"
