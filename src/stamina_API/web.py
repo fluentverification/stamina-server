1e04c2edbb63444cb85c54bae5e7ed93edc3a6b8
Josh Jeppson <joshua.jeppson@usu.edu>
2023-06-20 16:00:08 -0600

    fixed error.html

--- src/stamina_API/web.py
+++ src/stamina_API/web.py
@@ -7,10 +7,10 @@ def create_html_for_response(response, job_status):
 	timeout=5
 	return render_template('response.html', uid=uid, status=status, timeout=timeout, Settings=Settings)
 
-def create_html_err(error_msg, redir=True, redir_url="https://staminachecker.org/api/myjobs", after_error_msg = ""):
+def create_html_err(error_msg, redir=True, redir_url="https://staminachecker.org/api/myjobs", after_error_msg = "", timeout=5):
 	redir_str = ""
 	redir_msg = ""
-	return render_template("error.html", redir_url=redir_url, redir=redir, error_msg=error_msg, after_error_msg=after_error_msg)
+	return render_template("error.html", redir_url=redir_url, redir=redir, error_msg=error_msg, after_error_msg=after_error_msg, timeout=timeout)
 
 def json_to_table(json):
 	html = "<table class=table>\n<tr><th>Key</th><th>Value</th></tr>"
